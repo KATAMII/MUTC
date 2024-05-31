1c11a304939d52a814626d1ab45ec6c93b771bc2
KATAMII <lydiamwaah01@gmail.com>
2024-06-01 00:40:56 +0300

    add testimonials

--- src/pages/Home/Testimonials.jsx
+++ src/pages/Home/Testimonials.jsx
@@ -1,6 +1,8 @@
 import React ,{useState,useEffect} from 'react'
 import './Testimonials.css'
 import lydiaImg from'../../assets/lydia.jpg'
+import miriumImg from'../../assets/mirium.jpg'
+import victorImg from'../../assets/victor.jpg'
 
 
 function Testimonials() {
@@ -10,14 +12,38 @@ function Testimonials() {
         <div className='title'>
             <h2>Testimonials</h2>
         </div>
-        <div className='section-center'>
-            <img src={lydiaImg} alt="lydia a student" />
-            <h3 className='name-para'>Lydia wambui</h3>
+        <div className='section-testimonal'>
+            <div className="section2">
+                <div className='section-center'>
+                    <img src={lydiaImg} alt="lydia a student" />
+                    <h3 className='name-para'>Lydia wambui</h3>
+                    <h2 className='name-para1'>*Student*</h2>
+                </div>
+                <div className='testimony'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur pariatur eius officiis velit quibusdam possimus adipisci. Accusantium sit aperiam praesentium reprehenderit, nobis, repellat vero provident dignissimos ex inventore eos suscipit.</p>
+                </div>
+            </div>
+            <div className="section2" id='out'>
+                <div className='section-center'>
+                    <img src={miriumImg} alt="Mirium a VC" />
+                    <h3 className='name-para'>Mirium Mugure</h3>
+                    <h2 className='name-para1'>*COD*</h2>
+                </div>
+                <div className='testimony'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur pariatur eius officiis velit quibusdam possimus adipisci. Accusantium sit aperiam praesentium reprehenderit, nobis, repellat vero provident dignissimos ex inventore eos suscipit.</p>
+                </div>
+            </div>
+            <div className="section2">
+                <div className='section-center'>
+                    <img src={victorImg} alt="Victor a student leaader" />
+                    <h3 className='name-para'>Victor Tanui</h3>
+                    <h2 className='name-para1'>*Leader*</h2>
+                </div>
+                <div className='testimony'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur pariatur eius officiis velit quibusdam possimus adipisci. Accusantium sit aperiam praesentium reprehenderit, nobis, repellat vero provident dignissimos ex inventore eos suscipit.</p>
+                </div>
+            </div>
         </div>
-        <div className='testimony'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur pariatur eius officiis velit quibusdam possimus adipisci. Accusantium sit aperiam praesentium reprehenderit, nobis, repellat vero provident dignissimos ex inventore eos suscipit.</p>
-        </div>
-
 
     </section>
   )
