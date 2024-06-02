f2b57afd518400e6da18d417cb65b9ccc6b1a6c3
KATAMII <lydiamwaah01@gmail.com>
2024-06-02 12:46:57 +0300

    add events and style them

--- src/pages/Events/events.jsx
+++ src/pages/Events/events.jsx
@@ -1,9 +1,60 @@
 import React from "react";
+import './events.css'
+import { MdOutlineSecurity } from "react-icons/md";
+import { GrAppsRounded } from "react-icons/gr";
+import { CgWebsite } from "react-icons/cg";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { MdOutlineEventNote } from "react-icons/md";
+import { GrCloudComputer } from "react-icons/gr";
+import { FaAws } from "react-icons/fa";
+
 
 function Events() {
   return (
-    <div>
-      <h1>events</h1>
+    <div className="eventssection">
+      <div className="bannerSection">
+        <h1 className="Ourleaders">Weekly Events</h1>
+        <p className="qualified">View Our Events </p>
+      </div>
+      <div className="ourevents">
+        <div className="event">
+          <p className="eventicon">< MdOutlineSecurity/></p>
+          <h3 className="eventtitle"><FaRegCalendarAlt/> Monday 4pm to 7pm</h3>
+          <p className="eventdesc">Cyber Security</p>
+          <p className="lead">Lead: Webster Ifedha</p>
+        </div>
+        <div className="event">
+          <p className="eventicon">< MdOutlineSecurity/></p>
+          <h3 className="eventtitle"><FaRegCalendarAlt/> Monday 4pm to 7pm</h3>
+          <p className="eventdesc">Cyber Security</p>
+          <p className="lead">Lead: Webster Ifedha</p>
+        </div>
+        <div className="event">
+          <p className="eventicon">< MdOutlineSecurity/></p>
+          <h3 className="eventtitle"><FaRegCalendarAlt/> Monday 4pm to 7pm</h3>
+          <p className="eventdesc">Cyber Security</p>
+          <p className="lead">Lead: Webster Ifedha</p>
+        </div>
+        <div className="event">
+          <p className="eventicon">< MdOutlineSecurity/></p>
+          <h3 className="eventtitle"><FaRegCalendarAlt/> Monday 4pm to 7pm</h3>
+          <p className="eventdesc">Cyber Security</p>
+          <p className="lead">Lead: Webster Ifedha</p>
+        </div>
+        <div className="event">
+          <p className="eventicon">< MdOutlineSecurity/></p>
+          <h3 className="eventtitle"><FaRegCalendarAlt/> Monday 4pm to 7pm</h3>
+          <p className="eventdesc">Cyber Security</p>
+          <p className="lead">Lead: Webster Ifedha</p>
+        </div>
+        <div className="event">
+          <p className="eventicon">< MdOutlineSecurity/></p>
+          <h3 className="eventtitle"><FaRegCalendarAlt/> Monday 4pm to 7pm</h3>
+          <p className="eventdesc">Cyber Security</p>
+          <p className="lead">Lead: Webster Ifedha</p>
+        </div>
+      </div>
+
     </div>
   );
 }
